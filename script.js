fa4a62efc6067bbcf3fb7aa16acfca76674b5925
Andrew Kullman <agkullman@protonmail.com>
2023-10-13 11:38:06 -0800

    Added Different Colors

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var container = document.createElement('div');
+const container = document.createElement('div');
 container.style.width = "100%";
+container.style.backgroundColor = 'slate grey'
 document.body.append(container);
+const boardHeading = document.createElement('h1')
+boardHeading.textContent = "CHESS BOARD"
+boardHeading.style.textAlign = "center"
+container.appendChild(boardHeading)
 
 for(var i = 0; i < 8; i++){
     for(var j = 0; j < 8; j++){
@@ -8,36 +13,33 @@ for(var i = 0; i < 8; i++){
         var div = document.createElement('div');
         if(i % 2 !== 0){
             if(j % 2 !== 0){
-                div.style.width = '11.1%';
+                div.style.width = '12.5%';
                 div.style.float = 'left';
-                div.style.paddingBottom = '11.1%'
-                div.style.background = 'red';
-                div.style.border = '1px solid black';
+                div.style.paddingBottom = '12.5%'
+                div.style.background = 'grey';
                 container.append(div);
             } else {
-                div.style.width = '11.1%';
+                div.style.width = '12.5%';
                 div.style.float = 'left';
-                div.style.paddingBottom = '11.1%'
-                div.style.background = 'black';
-                div.style.border = '1px solid black';
+                div.style.paddingBottom = '12.5%'
+                div.style.background = 'brown';
                 container.append(div);
             }
         } else {
             if(j === 0 || j % 2 === 0){
-                div.style.width = '11.1%';
+                div.style.width = '12.5%';
                 div.style.float = 'left';
-                div.style.paddingBottom = '11.1%'
-                div.style.background = 'red';
-                div.style.border = '1px solid black';
+                div.style.paddingBottom = '12.5%'
+                div.style.background = 'grey';              
                 container.append(div);
             } else {
-                div.style.width = '11.1%';
+                div.style.width = '12.5%';
                 div.style.float = 'left';
-                div.style.paddingBottom = '11.1%'
-                div.style.background = 'black';
-                div.style.border = '1px solid black';
+                div.style.paddingBottom = '12.5%'
+                div.style.background = 'brown';            
                 container.append(div);
             }
         }
     }
 }
+
